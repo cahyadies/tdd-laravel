d433fee8c5ea809a71239b3831531e0bd5bb97e5
Eko Sugeng Cahyadi <ekosugengcahyadi@gmail.com>
2021-10-11 09:47:32 +0700

    Adding Laravel Projects

---
+++ tests/Unit/SecondTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class SecondTest extends TestCase
+{
+
+    public function test_example()
+    {
+        // $this->assertTrue(true);
+
+        $this->get("/seconds")->assertStatus(200);
+    }
+}
